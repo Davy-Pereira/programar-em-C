d383bcb4b1042e43a69b88f14f5e5d5c2e842993
Davy-Pereira <davytrabalho34@gmail.com>
2026-08-27 00:00:09 -0300

    testando codigo

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+struct User{
+    char usuario[20];
+    char senha [20];
+};
+
 int main(){
+
+    struct User user;
+
 
     printf("Hello, world\n");
 
     return 0;
-    
+
 }
