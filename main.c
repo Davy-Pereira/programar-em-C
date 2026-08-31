0cbd97f5eb4aac25f741da71e6bc5ce90a5d5f7c
Davy-Pereira <davytrabalho34@gmail.com>
2026-08-31 11:22:11 -0300

    code time

--- main.c
+++ main.c
@@ -1,42 +1,64 @@
+// #include <stdio.h>
+// #include <string.h>
+
+
+// struct User{
+//     char usuario[20];
+//     char senha [20];
+// };
+
+// struct Login{
+//     char user[20];
+//     char password[20];
+// };
+
+// int main(){
+
+//     struct User user;
+//     struct Login login;
+
+//     strcpy(user.usuario, "admin");
+//     strcpy(user.senha, "123");
+    
+
+
+//     printf("Digite seu nome:\n");
+//     scanf("%s", &login.user);
+
+//     printf("Digite sua senha:\n");
+//     scanf("%s", &login.password);
+
+
+//     if (strcmp(user.usuario, login.user) == 0 && strcmp(user.senha, login.password) == 0){
+//         printf("Bem vindo, %s", login.user);
+//     } else {
+//         printf("Usuario ou senha incorretos");
+//     }
+
+//     return 0;
+
+// }
+
+
 #include <stdio.h>
-#include <string.h>
+#include <time.h>
 
+int main() {
 
-struct User{
-    char usuario[20];
-    char senha [20];
-};
+    time_t agora = time(NULL);
+    struct tm *data = localtime(&agora);
+    int hora = data->tm_hour;
 
-struct Login{
-    char user[20];
-    char password[20];
-};
-
-
-
-int main(){
-
-    struct User user;
-    struct Login login;
-
-    strcpy(user.usuario, "admin");
-    strcpy(user.senha, "123");
-    
-
-
-    printf("Digite seu nome:\n");
-    scanf("%s", &login.user);
-
-    printf("Digite sua senha:\n");
-    scanf("%s", &login.password);
-
-
-    if (strcmp(user.usuario, login.user) == 0 && strcmp(user.senha, login.password) == 0){
-        printf("Bem vindo, %s", login.user);
+    if (hora >= 6 && hora < 12) {
+        printf("Bom dia!\n");
+    } else if (hora >= 12 && hora < 18) {
+        printf("Boa tarde!\n");
+    } else if (hora >= 18 && hora < 24){
+        printf("Boa noite!\n");
     } else {
-        printf("Usuario ou senha incorretos");
+        printf("Boa madrugada!\n");
     }
-    
-    return 0;
 
+
+    return 0;
 }
